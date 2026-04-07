eb53894a0f9c7751730f5486857cd93ec4a8e621
Elias Huhsovitz <elias.tu512@gmail.com>
2026-04-07 03:20:47 +0200

    36: remove class, because python is extremely slow calling class functions

--- 36-valid-sudoku.py
+++ 36-valid-sudoku.py
@@ -1,57 +1,39 @@
 #!/usr/bin/env python
-class SudokuSet:
-    ascii_val_zero = ord('0')
-
-    def __init__(self):
-        self.arr = [False] * 10
-
-    def clear(self):
-        self.arr = [False] * 10
-
-    def add(self, c: str):
-        self.arr[ord(c) - self.ascii_val_zero] = True
-
-    def contains(self, c: str):
-        return self.arr[ord(c) - self.ascii_val_zero]
-
 class Solution:
-    # somehow still very slow, TODO: improve runtime
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        ZERO = ord('0')
+
         SUB_BOX_WIDTH = 3
         SUB_BOX_HEIGHT = 3
 
-        num_rows = len(board)
-        num_cols = len(board[0])
+        num_rows = 9
+        num_cols = 9
 
         # no list for rows needed, since we can clear the set each iteration of the outer for loop thats looping through the rows
-        seen_vals_row = SudokuSet()
-        seen_vals_columns = [SudokuSet() for _ in range(num_cols)]
-        seen_vals_sub_boxes = [[SudokuSet() for _ in range(SUB_BOX_WIDTH)] for _ in range(SUB_BOX_HEIGHT)]
+        rows = [[False] * 10 for _ in range(num_rows)]
+        cols = [[False] * 10 for _ in range(num_cols)]
+        boxes = [[False] * 10 for _ in range(num_cols)]
 
         for row in range(num_rows):
-            seen_vals_row.clear()
             for col in range(num_cols):
-                val = board[row][col]
-                if val == '.':
+                elem = board[row][col]
+                if elem == '.':
                     continue
 
+                # convert elem to int, so we can use it as index in primitive set (i.e. an array)
+                idx = ord(elem) - ZERO
+
+                box_idx = row // SUB_BOX_HEIGHT * SUB_BOX_WIDTH + col // SUB_BOX_WIDTH
+
                 # check if value is unique in this row
-                if seen_vals_row.contains(val):
-                    return False
-
                 # check if value is unique in this column
-                if seen_vals_columns[col].contains(val):
+                # check if value is uniqe in this sub box
+                if rows[row][idx] or cols[col][idx] or boxes[box_idx][idx]:
                     return False
 
-                # check if value is uniqe in this sub box:
-                sub_box_row = row // SUB_BOX_HEIGHT
-                sub_box_col = col // SUB_BOX_WIDTH
-                if seen_vals_sub_boxes[sub_box_row][sub_box_col].contains(val):
-                    return False
-
-                seen_vals_row.add(val)
-                seen_vals_columns[col].add(val)
-                seen_vals_sub_boxes[sub_box_row][sub_box_col].add(val)
+                rows[row][idx] = True
+                cols[col][idx] = True
+                boxes[box_idx][idx] = True
 
         return True
 
