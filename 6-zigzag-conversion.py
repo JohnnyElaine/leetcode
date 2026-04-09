d077788fc9793aee42cf0f25251f71a670ced4e0
Elias Huhsovitz <elias.tu512@gmail.com>
2026-04-09 02:41:07 +0200

    6-zigzag-conversion: improve efficiency by eliminating empty elements in grid

--- 6-zigzag-conversion.py
+++ 6-zigzag-conversion.py
@@ -4,44 +4,24 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows <= 1:
             return s
-        
-        numCols = 0
 
-        if len(s) % 2 == 0:
-            numCols = len(s) // 2
-        else:
-            numCols = len(s) // 2 + 1
-
-        # row-major grid
-        grid = [''] * (numRows * numCols)
+        rows = [''] * numRows
 
         row = 0
-        col = 0
 
-        row_dir = 1
-        col_dir = 0
+        row_direction = 1
 
         for c in s:
-            grid[row * numCols + col] = c
+            rows[row] += c
 
             if row == numRows -1:
-                row_dir = -1
-                col_dir = 1
-
+                row_direction = -1
             elif row == 0:
-                row_dir = 1
-                col_dir = 0
+                row_direction = 1
             
-            row += row_dir
-            col += col_dir
+            row += row_direction
 
-        res = ''
-
-        for row in range(numRows):
-            for col in range(numCols):
-                res += grid[row * numCols + col]
-        
-        return res
+        return "".join(rows)
         
 
 s = Solution()
