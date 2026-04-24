5444709e6cd43fa34cc4a70204141ca9c761fc56
Elias Huhsovitz <elias.tu512@gmail.com>
2026-04-24 17:21:34 +0200

    solve 66

---
+++ 66-Plus-One.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = 1
+        for i in range(len(digits) - 1, -1, -1):
+            sum = digits[i] + carry
+            digits[i] = sum % 10
+            carry = sum // 10
+            if carry == 0:
+                break
+
+        if carry > 0:
+            digits.insert(0, 1)
+
+        return digits
